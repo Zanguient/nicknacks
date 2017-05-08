037b9c2a557451d9fd3f87c40ccdefb40a907978
calvintwr <calvin14@gmail.com>
2017-05-08 21:05:21 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -60,7 +60,7 @@ console.log(process.env.STRIPE_SIMPLE_TOKEN);
 
 router.get('/requestToken', function(req, res) {
 
-    if(process.env.QBO_ALLOW_GET_FRESH_TOKEN !== 'true') return res.status(400).send();
+    if(process.env.QBO_ALLOW_GET_FRESH_TOKEN !== 'true') return res.status(403).send();
 
     rp({
         method: 'POST',
@@ -74,11 +74,15 @@ router.get('/requestToken', function(req, res) {
         json: true
     }).then(function (response) {
         
-        console.log(response);
-
         var requestToken = qs.parse(response);
 
-        console.log(requestToken)
+        if (!requestToken.oauth_token || !requestToken.oauth_token_secret) return res.status(400).send('Unable to get 2nd leg token from QBO API.');
+
+        // attach session with secret
+        req.session.oauth_token_secret = requestToken.oauth_token_secret
+
+        // redirect to QBO authorisation
+        res.redirect(QuickBooks.APP_CENTER_URL + requestToken.oauth_token);
 
     }).catch(function (err) {
         
@@ -92,59 +96,78 @@ router.get('/requestToken', function(req, res) {
 
     });
 
-
-
-    // request.post(postBody, function (e, r, data) {
-    //     var requestToken = qs.parse(data)
-    //     req.session.oauth_token_secret = requestToken.oauth_token_secret
-    //     res.redirect(QuickBooks.APP_CENTER_URL + requestToken.oauth_token);
-    // });
-})
+});
 
 router.get('/callback', function(req, res) {
-    console.log(req.query);
 
-    var postBody = {
-        url: QuickBooks.ACCESS_TOKEN_URL,
+    if(process.env.QBO_ALLOW_GET_FRESH_TOKEN !== 'true') return res.status(400).send();
+
+    if(req.query.realmId !== process.env.qbo_realmID) return res.status(400).send('The server is not value for the company you have selected.');
+
+    rp({
+        method: 'POST',
+        uri: QuickBooks.ACCESS_TOKEN_URL,
+        body: {},
         oauth: {
             consumer_key:    process.env.qbo_consumerKey,
             consumer_secret: process.env.qbo_consumerSecret,
             token:           req.query.oauth_token,
             token_secret:    req.session.oauth_token_secret,
             verifier:        req.query.oauth_verifier,
-            realmId:         req.query.realmId
+            realmId:         process.env.qbo_realmID
+        },
+        json: true
+    }).then(function (response) {
+
+        var accessToken = qs.parse(response);
+ 
+        global.QBO_ACCESS_TOKEN = accessToken.oauth_token;
+        global.QBO_ACCESS_TOKEN_SECRET = accessToken.oauth_token_secret;
+
+        // save the token
+        return DB.Token.findOrCreate({
+
+            where: { TokenID: 1 },
+
+            defaults: { data: accessToken }
+
+        });
+
+    }).spread(function(token, created) {
+
+        // save the access token somewhere on behalf of the logged in user
+        global.QBO = new QuickBooks(
+            process.env.qbo_consumerKey,
+            process.env.qbo_consumerSecret,
+            accessToken.oauth_token,
+            accessToken.oauth_token_secret,
+            process.env.qbo_realmID,
+            false, // use the Sandbox
+            true
+        ); // turn debugging on
+
+
+        // test out account access
+        QBO.findAccounts(function(_, accounts) {
+          accounts.QueryResponse.Account.forEach(function(account) {
+            console.log(account.Name)
+          })
+        });
+
+        res.send('success');
+
+    }).catch(function (err) {
+        
+        if (err.statusCode) {
+            res.status(err.statusCode)
+        } else {
+            res.status(500);
         }
-    }
+        
+        res.send(err);
 
-  request.post(postBody, function (e, r, data) {
-    var accessToken = qs.parse(data);
-    console.log(accessToken)
-    console.log(postBody.oauth.realmId)
+    });
 
-    if (e) {
-
-    }
-
-    global.QBO_ACCESS_TOKEN = accessToken.oauth_token;
-    global.QBO_ACCESS_TOKEN_SECRET = accessToken.oauth_token_secret;
-
-    // save the access token somewhere on behalf of the logged in user
-    global.QBO = new QuickBooks(process.env.qbo_consumerKey,
-                         process.env.qbo_consumerSecret,
-                         accessToken.oauth_token,
-                         accessToken.oauth_token_secret,
-                         postBody.oauth.realmId,
-                         false, // use the Sandbox
-                         true); // turn debugging on
-
-    // test out account access
-    QBO.findAccounts(function(_, accounts) {
-      accounts.QueryResponse.Account.forEach(function(account) {
-        console.log(account.Name)
-      })
-    })
-  });
-  res.send('<!DOCTYPE html><html lang="en"><head></head><body><script>window.opener.location.reload(); window.close();</script></body></html>')
 })
 
 router.get('/accounts', function(req, res, next) {
