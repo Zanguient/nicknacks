c5896c35ab1ac42e5d4d35818959258e0178eff9
calvintwr <calvin14@gmail.com>
2017-05-08 19:58:41 +0800

    Token model

---
+++ models/Token.js
@@ -0,0 +1,24 @@
+function Token(sequelize, DataTypes) {
+
+    var Token = sequelize.define('Token', {
+        TokenID: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        data: {
+            type: DataTypes.JSON,
+            allowNull: true
+        }
+    }, {
+        timestamps: true,
+        tableName: 'Token',
+        instanceMethods: {},
+        getterMethods: {},
+        classMethods: {}
+    });
+    return Token;
+};
+
+module.exports = Token;
