c3646a4f52964b73d168565b1728e4b068ba7078
calvintwr <calvin14@gmail.com>
2017-05-10 22:05:54 +0800

    amendments to refresh token.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('dotenv').load();
 //load models the last because it has dependencies on the previous globals.
 global.DB = require('./models/index.js');
 
+global.serverStatus = 'All OK';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -51,46 +53,50 @@ setInterval(retrieveTokenAndRefresh, 6.048e+8);
 
 function retrieveTokenAndRefresh() {
     // get the token
-    DB.Token.findById(1)
-        .then(function (token) {
-            // check validity of token data.
-            if (!token || !token.data || !token.data.oauth_token || !token.data.oauth_token_secret) {
-                throw new Error('CRITICAL: Obtaining token from database failed.');
-            }
+    DB.Token.findById(1).then(function (token) {
 
-            // save token to global variables
-            global.QBO_ACCESS_TOKEN = token.data.oauth_token;
-            global.QBO_ACCESS_TOKEN_SECRET = token.data.oauth_token_secret;
+        // check validity of token data.
+        if (!token || !token.data || !token.data.oauth_token || !token.data.oauth_token_secret) {
+            throw { message: 'CRITICAL: Obtaining token from database failed.' };
+        }
 
-            // send request to refresh token
-            return refreshQBOToken();
+        // save token to global variables
+        global.QBO_ACCESS_TOKEN = token.data.oauth_token;
+        global.QBO_ACCESS_TOKEN_SECRET = token.data.oauth_token_secret;
 
-        })
-        .then(function (accessToken) {
-            // save the token
-            return DB.Token.findOrCreate({
-                where: {
-                    TokenID: 1
-                },
-                defaults: {
-                    data: accessToken
-                }
-            });
-        })
-        .catch(function (err) {
-            // log the error
-            console.log(err);
-        })
+        // send request to attempt to refresh token
+        return refreshQBOToken();
 
-}
+    }).then(function() {
 
-// connect to quickbooks to refresh token
-function refreshQBOToken() {
-    // return a promise
-    return new Promise(function (resolve, reject) {
+        // initialise quickbooks
+        return global.QBO = new QuickBooks(
+            process.env.qbo_consumerKey,
+            process.env.qbo_consumerSecret,
+            global.QBO_ACCESS_TOKEN.oauth_token,
+            global.QBO_ACCESS_TOKEN_SECRET.oauth_token_secret,
+            process.env.qbo_realmID,
+            false, // use the Sandbox
+            true // turn debugging on
+        ); 
+
+    }).catch(function (err) {
+        // log the error
+        console.log(err);
+        global.serverStatus = err;
+
+    });
+
+
+    // connect to quickbooks to refresh token. returns a promise
+    function refreshQBOToken() {
+
         if (!global.QBO_ACCESS_TOKEN || !global.QBO_ACCESS_TOKEN_SECRET) throw new Error('CRITICAL: Failed to initalise tokens to the global namespace.');
 
-        rp({
+        // send the request to quickbooks
+        // this promise is returned to #retrieveTokenAndRefresh 
+        return rp({
+
             method: 'GET',
             uri: QuickBooks.APP_CENTER_BASE + '/api/v1/connection/reconnect',
             oauth: {
@@ -100,56 +106,65 @@ function refreshQBOToken() {
                 token: global.QBO_ACCESS_TOKEN
             },
             json: true
-        })
-            .then(function (response) {
 
-                // set the responseParsed store
-                var responseParsed = {};
+        }).then(function (response) {
 
-                // check if the response is not an Object
-                if (typeof response === 'string') {
-                    parseString(response, function (err, result) {
-                        if (err) {
-                            throw new Error('Unable to receive valid XML response.')
-                        }
-                        // assign the parsedResponse
-                        responseParsed = result.ReconnectResponse ? result.ReconnectResponse : result.PlatformResponse;
-                    });
-                }
+            // set the responseParsed store
+            var responseParsed = {};
 
-                // check if there is an error
-                if (parseInt(responseParsed.ErrorCode[0]) !== 0) {
-                    // throw error with the message
-                    throw new Error(responseParsed.ErrorMessage);
-                }
+            // check if the response is not an Object
+            if (typeof response === 'string') {
+                parseString(response, function (err, result) {
+                    if (err) {
+                        throw new Error('Unable to parse valid XML response.')
+                    }
+                    // assign the parsedResponse
+                    responseParsed = result.ReconnectResponse ? result.ReconnectResponse : result.PlatformResponse;
+                });
+            }
 
-                // check for tokens
-                if (!responseParsed.oauth_token[0] || !responseParsed.oauth_token_secret[0]) {
-                    throw new Error('Unable to get 2nd leg token from QBO API.');
-                }
+            // check if there is an error
+            var errorCode = parseInt(responseParsed.ErrorCode[0]);
 
-                // attach session with secret
-                req.session.oauth_token_secret = requestToken.oauth_token_secret;
+            if (errorCode === 212) {
+
+                // token is not refreshed, return nothing, life goes on...
+                return false;
+
+            } else if (errorCode === 0) {
+
+                // token is refreshed. need update the globals and save to DB
 
                 var token = {
-                    oauth_token: responseParsed.OAuthToken[0],
-                    oauth_token_secret: responseParsed.OAuthTokenSecret[0]
+                    "oauth_token_secret": responseParsed.OAuthTokenSecret[0],
+                    "oauth_token": responseParsed.OAuthToken[0]
                 };
 
-                // resolve the promise
-                resolve(token);
+                // set globals to the newly retrieved tokens
+                global.QBO_ACCESS_TOKEN = responseParsed.OAuthToken[0];
+                global.QBO_ACCESS_TOKEN_SECRET = responseParsed.OAuthTokenSecret[0];
 
-            })
-            .catch(function (err) {
-                // reject the promise
-                reject(err);
-            });
-    });
+                // refreshed token is updated to DB
+                return DB.Token.update({
+                    data: token
+                }, { 
+                    where: {
+                        TokenID: 1
+                    }
+                });
+
+            } else {
+
+                // any other errorCode, print out the error.
+                throw { message: 'Error code: ' + responseParsed.ErrorCode[0] + ' Message: ' + responseParsed.ErrorMessage[0] };
+            
+            }
+
+        });
+
+    }
+
 }
-
-
-// refresh the QBO token
-//refreshQBOToken();
 
 
 // catch 404 and forward to error handler
