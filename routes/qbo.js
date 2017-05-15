2b6cee4dad1c190f70ffb15ac48d472e6172400f
calvintwr <calvin14@gmail.com>
2017-05-15 22:56:15 +0800

    .

--- routes/qbo.js
+++ routes/qbo.js
@@ -92,10 +92,12 @@ router.get('/callback', function(req, res) {
         console.log(11111)
         // if not created, update the current token
         if (!created) {
-            return DB.Token.update({ data: _ACCESS_TOKEN }, {
-                where: { TokenID: 1},
-                limit: 1
-            });
+            console.log(_ACCESS_TOKEN);
+            return token.updateAttributes({ data: _ACCESS_TOKEN });
+            // return DB.Token.update({ data: _ACCESS_TOKEN }, {
+            //     where: { TokenID: 1},
+            //     limit: 1
+            // });
             //return token.update({ data: _ACCESS_TOKEN }, { fields: ['data']});
             //return token.save({fields: ['data']});
         } else { return false; }
