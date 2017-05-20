a172c6b0ebbe32ab52803b23da90ad8a9ccd7083
calvintwr <calvin14@gmail.com>
2017-05-20 20:38:36 +0800

    .

--- apps/QBOPurchase.js
+++ apps/QBOPurchase.js
@@ -1,32 +1,30 @@
 var QBOPurchase = {
-    {
-      
-      "AccountRef": {
-        "value": "31",
-        "name": "Current"
-      },
-      "PaymentMethodRef": {
-        "value": "1"
-      },
-      "PaymentType": "Cash",
-      "EntityRef": {
-        "value": "28",
-        "name": "Stripe Commission",
-        "type": "Vendor"
-      },
-      "GlobalTaxCalculation": "TaxExcluded"
-      // "Line": [
-      //   {
-      //     "Amount": 10.00,
-      //     "DetailType": "AccountBasedExpenseLineDetail",
-      //     "AccountBasedExpenseLineDetail": {
-      //      "AccountRef": {
-      //         "name": "Meals and Entertainment",
-      //         "value": "13"
-      //       }
-      //     }
-      //   }
-      // ]
-    }
+  "AccountRef": {
+    "value": "31",
+    "name": "Current"
+  },
+  "PaymentMethodRef": {
+    "value": "1"
+  },
+  "PaymentType": "Cash",
+  "EntityRef": {
+    "value": "28",
+    "name": "Stripe Commission",
+    "type": "Vendor"
+  },
+  "GlobalTaxCalculation": "TaxExcluded"
+  // "Line": [
+  //   {
+  //     "Amount": 10.00,
+  //     "DetailType": "AccountBasedExpenseLineDetail",
+  //     "AccountBasedExpenseLineDetail": {
+  //      "AccountRef": {
+  //         "name": "Meals and Entertainment",
+  //         "value": "13"
+  //       }
+  //     }
+  //   }
+  // ]
+    
 }
 module.exports = QBOPurchase
