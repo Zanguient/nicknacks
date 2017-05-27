78704fd901d4d3161a6577c85dea1a00e4d15c1a
calvintwr <calvin14@gmail.com>
2017-05-28 00:06:16 +0800

    .

--- apps/calculateStripeCommissionAmountOnRefund.js
+++ apps/calculateStripeCommissionAmountOnRefund.js
@@ -1,7 +1,7 @@
 function calculateStripeCommissionAmountOnRefund(stripeObject) {
     
-    var total = parseInt(D.get(req, 'body.data.object.amount'));
-    var amountRefunded = parseInt(D.get(req, 'body.data.object.amount_refunded'));
+    var total = parseInt(D.get(stripeObject, 'body.data.object.amount'));
+    var amountRefunded = parseInt(D.get(stripeObject, 'body.data.object.amount_refunded'));
 
     // if it is full refund
     if (total === amountRefunded) return total/100;
