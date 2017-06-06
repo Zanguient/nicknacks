19f98e1a2104d0c6cc4bdd8adb76a4ffb27f5002
calvintwr <calvin14@gmail.com>
2017-06-06 17:41:09 +0800

    fixed DocNumber max length of 21 allowable by QBO

--- apps/QBOJournalPayoutPaid.js
+++ apps/QBOJournalPayoutPaid.js
@@ -2,7 +2,7 @@ function QBOJournalPayoutPaid(object) {
 
   
   var QBOJournalObject = {
-      "DocNumber": object.id,
+      "DocNumber": object.id.substring(0,21), // DocNumber max string length is 21.
       "TxnDate": MOMENT.unix(D.get(object, 'data.object.arrival_date')).format('YYYY-MM-DD'),
       "PrivateNote": "Stripe Payout (" + parseInt(object.data.object.amount)/100 + ")"
   };
