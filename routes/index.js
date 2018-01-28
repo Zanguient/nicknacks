181faac0eaf469d5c50bc4b2a6a1e4e950b302d8
calvintwr <calvin14@gmail.com>
2018-01-28 19:44:42 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var rp = require('request-promise');
 /* SAFARI/IOS Bug */
 router.all('*', function (req, res, next) {
   agent = req.headers['user-agent'];
-  if (agent.indexOf('Safari') > -1 && agent.indexOf('Chrome') === -1 &&& agent.indexOf('OPR') === -1) {
+  if (agent.indexOf('Safari') > -1 && agent.indexOf('Chrome') === -1 && agent.indexOf('OPR') === -1) {
     res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
     res.header('Pragma', 'no-cache');
     res.header('Expires', 0);
