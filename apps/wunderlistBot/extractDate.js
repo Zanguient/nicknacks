ad9234f0bb1ee61abad6c610572df567aacd7404
calvintwr <calvin14@gmail.com>
2018-06-02 22:54:45 +0800

    made more versatile to accept other date formats

--- apps/wunderlistBot/extractDate.js
+++ apps/wunderlistBot/extractDate.js
@@ -9,7 +9,36 @@ function extractDate($body, format) {
 
     if (dateOfDeliveryRaw === undefined) return false;
 
-    dateOfDelivery = moment(dateOfDeliveryRaw, "Do MMMM YYYY, dddd");
+    // use the comma in the substring and day to see if it is the custom full date convention
+    // of 15th February 2018, Thursday
+    function checkIsCustomFormat(dateString) {
+
+        // if dateString doesn't have a comma, assume it is not the custom format
+        if (dateString.indexOf(',') === -1) return false;
+
+        // if comma is found, do some more check
+        let days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+        var found = false;
+
+        for(let i=0; i<days.length; i++) {
+            if (dateString.indexOf(days[i]) !== -1) {
+                found = true;
+                break
+            }
+        }
+
+        return found;
+    }
+
+
+    if (checkIsCustomFormat(dateOfDeliveryRaw)) {
+        dateOfDelivery = moment(dateOfDeliveryRaw, "Do MMMM YYYY, dddd");
+    } else {
+
+        // if not just attempt to format it anyway
+        dateOfDelivery = moment(dateOfDeliveryRaw);
+    }
 
     if (dateOfDelivery.format() === 'Invalid date') {
         console.log('WARN: Invalid date format detected: ' + dateOfDeliveryRaw);
