8ee08b91f3aaaac06d4fa6e849164994e551c096
calvintwr <calvin14@gmail.com>
2018-07-07 19:00:33 +0800

    added magento web hook checkout

--- routes/api/v2/magento-webhooks/index.js
+++ routes/api/v2/magento-webhooks/index.js
@@ -1,46 +1,87 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 router.post('/checkout', function (req, res) {
 
     if(D.get(req, 'body.livemode') === false) {
-        console.log(req.body);
-        return res.send({ success: true });
+        console.log(req.body)
+        return res.send({ success: true })
     }
 
-    if(req.query.token !== process.env.STRIPE_SIMPLE_TOKEN) return res.status(403).send();
+    if(D.get(req, 'body.type') && !D.get(req, 'body.type').toLowerCase() === 'order') {
+        return res.status(400).send({
+            success: false,
+            message: 'This endpoint accepts `order` type calls only.',
+            hideMessage: false,
+            debug: {
+                message: 'This endpoint accepts `order` type calls only.',
+                error: null
+            }
+        })
+    }
+
+    if(req.query.token !== process.env.STRIPE_SIMPLE_TOKEN) return res.status(403).send()
 
     // get sales order number
-    var salesOrderNumber = D.get(req, 'body.data.object.description');
+    var salesOrderNumber = D.get(req, 'body.increment_id')
 
     if(!salesOrderNumber) {
-        return res.status(400).send({ success: false, error: { message: 'unable to parse sales order number.'} });
-    } else {
-        salesOrderNumber = salesOrderNumber.split(',')[0].trim();
+        return res.status(400).send({
+            success: false,
+            message: 'Missing `increment_id` parameter as salesOrderNumber.',
+            hideMessage: false,
+            debug: {
+                message: 'Missing `increment_id` parameter as salesOrderNumber.',
+                error: null
+            }
+        })
     }
 
+    // get paymentMethod
+    var paymentMethod = D.get(req, 'body.data.payment_method')
+
+    if(!paymentMethod) {
+        return res.status(400).send({
+            success: false,
+            message: 'Missing `payment_method` parameter.',
+            hideMessage: false,
+            debug: {
+                message: 'Missing `payment_method` parameter.',
+                error: null
+            }
+        })
+    }
 
     // save the data
     return DB.Transaction.create({
+
         data: req.body,
         status: 'pending',
         eventType: 'checkout',
         salesOrderNumber: salesOrderNumber,
-        eventId: req.body.id
-    })
-    .then(function (transaction) {
+        paymentMethod: paymentMethod
+
+    }).then(function (transaction) {
         // send success
         return res.send({
             success: true
-        });
-    })
-    .catch(function (err) {
-        // log the error
-        console.log("CRITICAL: Failed to capture checkout with error: " + err);
-        res.status(500).send();
-    });
+        })
 
-});
+    }).catch(function (err) {
+        // log the error
+        console.log("CRITICAL: Failed to capture checkout with error: " + err)
+        res.status(500).send({
+            success: false,
+            message: 'Server error in creating `Transaction` DB record.',
+            hideMessage: false,
+            debug: {
+                message: 'Server error in creating `Transaction` DB record.',
+                error: JSON.stringify(err)
+            }
+        })
+    })
+
+})
 
 
 
