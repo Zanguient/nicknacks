9b0cf690c00de7c8d7a02cbcaa4173158061b896
calvintwr <calvin14@gmail.com>
2018-08-28 02:11:57 +0800

    .

--- routes/api/v2/magento-webhooks/index.js
+++ routes/api/v2/magento-webhooks/index.js
@@ -47,6 +47,7 @@ router.post('/sales-order', (req, res, next) => {
             $or: {
                 salesOrderNumber: req.body.increment_id,
                 salesOrderNumber: '#' + req.body.increment_id
+            }
         }
     }).then(txn => {
 
